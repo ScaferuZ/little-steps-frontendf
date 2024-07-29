b1273948fdae320cce97e29b31d0fc23896a4bee
ScaferuZ <endrafruz@gmail.com>
2024-07-29 12:12:41 +0700

    feat: working on artikel screen

--- app/(main)/parent-pro/artikel.tsx
+++ app/(main)/parent-pro/artikel.tsx
@@ -1,14 +1,34 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable, ScrollView } from 'react-native'
 import React from 'react'
 import NavWithSearch from 'src/components/NavWithSearch'
+import ScreenLayout from 'src/components/ScreenLayout'
+import ArtikelPreview from 'src/components/ArtikelPreview'
 
 const artikel = () => {
   return (
-    <View className="bg-lightPink pt-8 pb-4 rounded-b-[20px] -mb-40">
-      <View className="flex flex-col items-center justify-center mx-6 mb-44">
-        <NavWithSearch placeholder="Cari artikel atau video..." />
-      </View>
-    </View>
+    <ScreenLayout>
+      <ScrollView>
+        <View className="bg-lightPink pt-8 pb-4 rounded-b-[20px] -mb-40">
+          <View className="flex flex-col mx-6 mb-44">
+            <NavWithSearch placeholder="Cari artikel" />
+            <View className="flex flex-row justify-between mt-3">
+              <Pressable className="bg-[#FFCBDC] px-4 py-3 rounded-lg">
+                <Text className="text-primary font-medium">Popular</Text>
+              </Pressable>
+              <Pressable className="bg-white px-4 py-3 rounded-lg">
+                <Text className="text-black font-medium">Rekomendasi</Text>
+              </Pressable>
+              <Pressable className="bg-white px-4 py-3 rounded-lg">
+                <Text className="text-black font-medium">Artikel Terbaru</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+        <ArtikelPreview />
+        <ArtikelPreview />
+        <ArtikelPreview />
+      </ScrollView>
+    </ScreenLayout>
   )
 }
 
