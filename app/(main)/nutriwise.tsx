7c9648b1aaf604898e85a86d5a7369d76f15005c
ScaferuZ <endrafruz@gmail.com>
2024-08-06 15:41:59 +0700

    feat: nutriwise open camera function

--- app/(main)/nutriwise.tsx
+++ app/(main)/nutriwise.tsx
@@ -1,9 +1,20 @@
 import React, { useState } from 'react'
 import Navbar from 'src/components/Navbar'
-import { View, Text, Image, TouchableOpacity, FlatList, Dimensions, Pressable } from 'react-native'
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  Dimensions,
+  Pressable,
+  StyleSheet,
+  SafeAreaView
+} from 'react-native'
 import { Card, CardBody, CardTitle, CardActions } from 'src/components/shared/Card'
-import Button from 'src/components/Button'
 import Ionicons from '@expo/vector-icons/Ionicons'
+
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera'
 
 interface NutritionCardData {
   title: string
@@ -81,12 +92,42 @@ const PaginationDot: React.FC<{ active: boolean }> = ({ active }) => (
 
 const NutriwiseScreen: React.FC = () => {
   const [activeIndex, setActiveIndex] = useState(0)
+  const [cameraVisible, setCameraVisible] = useState(false)
+  const [facing, setFacing] = useState<CameraType>('back')
+  const [permission, requestPermission] = useCameraPermissions()
 
   const handleScroll = (event: any) => {
     const slideWidth = Dimensions.get('window').width * 0.9 + 20
     const offset = event.nativeEvent.contentOffset.x
     const index = Math.round(offset / slideWidth)
     setActiveIndex(index)
+  }
+
+  const openCamera = async () => {
+    if (!permission?.granted) {
+      const newPermission = await requestPermission()
+      if (!newPermission.granted) {
+        alert('Camera permission is required to use this feature')
+        return
+      }
+    }
+    setCameraVisible(true)
+  }
+
+  const closeCamera = () => {
+    setCameraVisible(false)
+  }
+
+  const toggleCameraFacing = () => {
+    setFacing((current) => (current === 'back' ? 'front' : 'back'))
+  }
+
+  if (cameraVisible) {
+    return (
+      <View className="flex justify-center">
+        <CameraView facing={facing} className="flex"></CameraView>
+      </View>
+    )
   }
 
   return (
@@ -96,7 +137,7 @@ const NutriwiseScreen: React.FC = () => {
           <Navbar title="Nutriwise" />
         </View>
       </View>
-      <View style={{ marginTop: -160 }}>
+      <View className="-mt-40">
         <FlatList
           data={nutritionCardData}
           renderItem={({ item }) => <NutritionCard item={item} />}
@@ -108,13 +149,15 @@ const NutriwiseScreen: React.FC = () => {
           onScroll={handleScroll}
         />
       </View>
-      <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 10 }}>
+      <View className="flex-row justify-center mt-2.5">
         {nutritionCardData.map((_, index) => (
           <PaginationDot key={index} active={index === activeIndex} />
         ))}
       </View>
       <View className="mt-8 mx-6">
-        <Pressable className="flex flex-row items-center justify-between bg-darkPink px-2 py-2 rounded-lg">
+        <Pressable
+          className="flex flex-row items-center justify-between bg-darkPink px-2 py-2 rounded-lg"
+          onPress={openCamera}>
           <Text className="flex-1 text-center text-white text-base">
             Scan Nutrisi Makanan Anakmu Sekarang
           </Text>
